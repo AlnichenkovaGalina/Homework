994198c4496347b5a98dfaf79780d09f51b92649
GalinaAlnichenkova <g.alnichenkova@gmail.com>
2022-08-07 01:24:04 +0400

    Zadacha 4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число N ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.WriteLine("Все четные числа от 1 до N ");
+while (count <= namber)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(" " + count);
+    }
+    count++;
+}
