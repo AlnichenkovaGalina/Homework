5008ebc862124d1f35154da8f44a00eaaf4e7040
Alnichenkova_Galina <g.alnichenkova@gmail.com>
2022-08-09 22:14:11 +0300

    Zadacha 5

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+System.Console.WriteLine("Введите трёхзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100 && n < 999)
+{
+    int num = n % 100;
+    int n1 = n / 100;
+    int n2 = num / 10;
+    int n3 = num % 10;
+    System.Console.WriteLine("Вторая цифра "+ n2);
+}
+
+
