04c1ba7bc30e03bffffa16080a6e2dee71a5fa22
Alnichenkova_Galina <g.alnichenkova@gmail.com>
2022-08-24 00:23:01 +0300

    Zadacha 15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//static void Main(int[] arrey)
+{
+    int[] array = new int[10];
+    int summ = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write(array[i] + " ");
+
+        if (i % 2 != 0)
+        {
+            summ = summ + array[i];
+        }
+    }
+    Console.WriteLine();
+    Console.Write($"Сумма элементов, стоящих на нечетных позициях: {summ}");
+}
+
