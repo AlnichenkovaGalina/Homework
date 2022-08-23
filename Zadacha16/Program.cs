38d82f5c91c5112e430771094c01a85a2377158c
Alnichenkova_Galina <g.alnichenkova@gmail.com>
2022-08-24 01:39:08 +0300

    Zadacha 16

---
+++ Zadacha16/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write(array[i] + " ");
+    }
+    int max = array[0];
+    int min = array[0];
+    {
+        for (int i = 1; i < array.Length; i++)
+            if (min > array[i])
+            {
+                min = array[i];
+            }
+            else if (max < array[i])
+            {
+                max = array[i];
+            }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Разницa между максимальным и минимальным элементов массива: {max - min}");
+
+}
+
+
