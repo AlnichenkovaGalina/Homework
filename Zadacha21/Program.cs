bbbe4e263a9a2111f5271c72c2c78a240a1ffae7
GalinaAlnichenkova <g.alnichenkova@gmail.com>
2022-08-29 01:46:27 +0400

    Zadacha 21

---
+++ Zadacha21/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+Console.WriteLine("Введите число строк m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Console.WriteLine();
+fillArrayRandom(array);
+writeArray(array);
+Console.WriteLine();
+
+void arithmeticMain(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double arithmetic = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            arithmetic = arithmetic + array[i, j];
+        }
+        arithmetic = arithmetic / m;
+        Console.Write(arithmetic + "; ");
+    }
+}
+Console.Write("Cреднее арифметическое элементов в каждом столбце: ");
+arithmeticMain(array);
+
+void fillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+void writeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
