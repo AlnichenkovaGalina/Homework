f4e36bf09db5903e9834e19c70d0c6eb0547c374
GalinaAlnichenkova <g.alnichenkova@gmail.com>
2022-08-28 23:13:51 +0400

    Zadacha 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите число строк m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+Console.WriteLine();
+fillArrayRandom(array);
+writeArray(array);
+
+
+void fillArrayRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+void writeArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
